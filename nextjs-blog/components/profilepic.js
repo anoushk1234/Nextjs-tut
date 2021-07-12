8f459a1651a06c32aeea409bc05fdb99aa4e346a
Anoushk Kharangate <anoushka1828@gmail.com>
2021-07-12 15:05:48 +0530

    2

--- nextjs-blog/components/profilepic.js
+++ nextjs-blog/components/profilepic.js
@@ -1,11 +1,16 @@
 import Image from 'next/image'
+import { Box } from '@chakra-ui/layout';
+
 const YourComponent = () => (
-    <Image
+  <Box borderRadius="2xl">
+   <Image 
+   
       src="/images/lighty.jpeg" // Route of the image file
       height={144} // Desired size with correct aspect ratio
       width={144} // Desired size with correct aspect ratio
       alt="Your Name"
-    />
+      
+    /></Box>
   )
 
   export default YourComponent;
